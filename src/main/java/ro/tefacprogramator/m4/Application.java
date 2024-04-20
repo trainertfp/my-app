dc7911885044ca8c81c36097d16624b16b48a4d2
Mihai Vilcu <mihai@gmail.com>
2024-04-20 11:34:45 +0300

    Fixed the labels and the multiply method.

--- src/main/java/ro/tefacprogramator/m4/Application.java
+++ src/main/java/ro/tefacprogramator/m4/Application.java
@@ -6,19 +6,19 @@ public class Application {
         System.out.println("New program");
 
         double rezPow = Calculator.pow(5, 4);
-        System.out.println("rez pow = " + rezPow);
+        System.out.println("rezult pow = " + rezPow);
 
         int rezAdd = Calculator.add(100, 45);
-        System.out.println("rez add = " + rezAdd);
+        System.out.println("rezult add = " + rezAdd);
 
         int rezSub = Calculator.subtract(100, 45);
-        System.out.println("rez subtract = " + rezSub);
+        System.out.println("rezult subtract = " + rezSub);
 
         double rezDiv = Calculator.divide(100, 45);
-        System.out.println("rez divide = " + rezDiv);
+        System.out.println("rezult divide = " + rezDiv);
 
-        double rezMul = Calculator.divide(100, 45);
-        System.out.println("rez multiply = " + rezMul);
+        double rezMul = Calculator.multiply(6, 7);
+        System.out.println("rezult multiply = " + rezMul);
 
     }
 }
