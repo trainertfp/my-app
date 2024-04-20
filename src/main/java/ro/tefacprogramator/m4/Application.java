624fba06e0d98b27a0eb1dcc9b814d9838e87022
Mihai Vilcu <mihai@gmail.com>
2024-04-20 10:31:46 +0300

    Initial commit

---
+++ src/main/java/ro/tefacprogramator/m4/Application.java
@@ -0,0 +1,8 @@
+package ro.tefacprogramator.m4;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ro/tefacprogramator/m4/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
